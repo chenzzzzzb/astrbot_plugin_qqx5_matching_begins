bbdcccd9f6677a59925c15a2f2d1098b41f21373
chenzzzzzb <laobin2020@gmail.com>
2025-08-18 17:54:50 +0800

    Update main.py
    修改指令

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("qqx5_matching_begins", "Chenzb", "一个简单的 匹配倒计时 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -12,12 +12,12 @@ class MyPlugin(Star):
         """插件初始化"""
 
     # 注册命令
-    @filter.command("countdown")
+    @filter.command("匹配倒计时")
     async def countdown(self, event: AstrMessageEvent):
         """倒计时 3-2-1-开始"""
         for i in [3, 2, 1, "开始！"]:
             yield event.plain_result(str(i))
-            await asyncio.sleep(0.6)  # 每条消息之间等待 1 秒
+            await asyncio.sleep(0.6)  # 每条消息之间等待 0.6 秒
 
     async def terminate(self):
         """插件销毁"""
